adf305dc31361bfb1425203fc1045d67a589b7cf
Bruno Assis <bruno.assis@live.com>
2022-01-05 14:07:39 -0300

    re arranging folders

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { HomeModule } from './app/home.module';
+import { HomeModule } from './app/home/home.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
