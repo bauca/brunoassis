174d979c9b8485ffe5c75371b9a63b8708f800c4
Bruno Assis <bruno.assis@live.com>
2022-01-09 01:28:53 -0300

    Navigation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app-component/app-component.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
     MenuComponent,
+    ContactComponent,
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [AppComponent]
 })
 
-export class HomeModule { }
+export class AppModule { }
