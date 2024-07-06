32b05e724996ed5a6bc1b86ae4b0e54d14d20eca
Bruno Assis <bruno.assis@live.com>
2024-07-06 20:38:02 +0200

    Renamed frontend to source

--- frontend/src/app/menu/menu.component.spec.ts
+++
@@ -1,7 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { MenuComponent } from './menu.component';
-
-describe('MenuComponent', () => {
-
-});
